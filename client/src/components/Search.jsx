91e5363ca5512ef5e7e8a0384c21c431524b5f2c
John Sangiolo <johnsangiolo@gmail.com>
2018-10-21 13:16:02 -0400

    updated Search.jsx to render genres to drop down list when component renders

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
@@ -9,6 +10,14 @@ class Search extends React.Component {
   }
   getGenres() {
     //make an axios request in this component to get the list of genres from your endpoint GET GENRES
+    axios.get('/genres')
+    .then( ({data}) => {
+      this.setState({genres: data.genres});
+    })
+  }
+
+  componentDidMount() {
+    this.getGenres();
   }
 
   render() {
@@ -16,14 +25,10 @@ class Search extends React.Component {
       <div className="search">
         <button onClick={() => {this.props.swapFavorites()}}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
         <br/><br/>
-
-        {/* Make the select options dynamic from genres !!! */}
-        {/* How can you tell which option has been selected from here? */}
-
         <select>
-          <option value="theway">The Way</option>
-          <option value="thisway">This Way</option>
-          <option value="thatway">That Way</option>
+          {this.state.genres.map( genre =>
+            <option key={genre.id} value={genre.id}>{genre.name}</option>  
+          )}
         </select>
         <br/><br/>
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios';
 // import AnyComponent from './components/filename.jsx'
 import Search from './components/Search.jsx'
 import Movies from './components/Movies.jsx'
